d55327b0fd55359816980f68e151848e9a30d464
Jim Lightfoot <jim.lightfoot@outlook.com>
2016-04-18 08:39:06 -0700

    Added files via upload

---
+++ src/Web/Cache.cs
@@ -0,0 +1,95 @@
+﻿/****************************************************************************/
+/*                                                                          */
+/*    The Mondo Libraries  	                                                */
+/*                                                                          */
+/*      Namespace: Mondo.Web	                                            */
+/*           File: Cache.cs                                                 */
+/*      Class(es): Cache                                                    */
+/*        Purpose: Web caching                                              */
+/*                                                                          */
+/*  Original Author: Jim Lightfoot                                          */
+/*    Creation Date: 29 Nov 2015                                            */
+/*                                                                          */
+/*   Copyright (c) 2015-2016 - Jim Lightfoot, All rights reserved           */
+/*                                                                          */
+/*  Licensed under the MIT license:                                         */
+/*    http://www.opensource.org/licenses/mit-license.php                    */
+/*                                                                          */
+/****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Caching;
+
+using Mondo.Common;
+
+namespace Mondo.Web
+{
+    /****************************************************************************/
+    /****************************************************************************/
+    public class Cache : BaseCache
+    {
+        /****************************************************************************/
+        public override object Get(string strKey)
+        {
+            return(HttpContext.Current.Cache.Get(strKey));
+        }
+
+        /****************************************************************************/
+        public override void Add(string strKey, object objToAdd)
+        {
+            Add(strKey, objToAdd, new TimeSpan(1, 0, 0));
+        }
+
+        /****************************************************************************/
+        public override void Add(string strKey, object objToAdd, DateTime dtExpires, ICacheDependency dependency = null)
+        {
+            try
+            { 
+                HttpContext.Current.Cache.Insert(strKey, objToAdd, ToCacheDependency(dependency), dtExpires, System.Web.Caching.Cache.NoSlidingExpiration);
+            }
+            catch
+            {
+            }
+        }
+
+        /****************************************************************************/
+        public override void Add(string strKey, object objToAdd, TimeSpan tsExpires, ICacheDependency dependency = null)
+        {
+            try
+            { 
+                HttpContext.Current.Cache.Insert(strKey, objToAdd, ToCacheDependency(dependency), System.Web.Caching.Cache.NoAbsoluteExpiration, tsExpires);
+            }
+            catch
+            {
+            }
+        }
+
+        /****************************************************************************/
+        public override void Remove(string strKey)
+        {
+            try
+            { 
+                HttpContext.Current.Cache.Remove(strKey);
+            }
+            catch
+            {
+            }
+        }
+
+        /****************************************************************************/
+        private static CacheDependency ToCacheDependency(ICacheDependency dependency)
+        {
+            if(dependency == null)
+                return(null);
+
+            if(dependency is FileDependency)
+                return(new CacheDependency((dependency as FileDependency).FileName));
+
+            // Don't understand any other types for now
+            return(null);
+
+        }
+    }
+}
---
+++ src/Web/WebContext.cs
@@ -0,0 +1,63 @@
+﻿/****************************************************************************/
+/*                                                                          */
+/*    The Mondo Libraries  	                                                */
+/*                                                                          */
+/*        Namespace: Mondo.Web							                    */
+/*             File: WebContext.cs											*/
+/*        Class(es): WebContext 							                */
+/*          Purpose: Provide an Http request context  	                    */
+/*                                                                          */
+/*  Original Author: Jim Lightfoot                                          */
+/*    Creation Date: 29 Nov 2015                                            */
+/*                                                                          */
+/*   Copyright (c) 2015-2016 - Jim Lightfoot, All rights reserved           */
+/*                                                                          */
+/*  Licensed under the MIT license:                                         */
+/*    http://www.opensource.org/licenses/mit-license.php                    */
+/*                                                                          */
+/****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+
+using Mondo.Common;
+
+namespace Mondo.Web
+{
+    /****************************************************************************/
+    /****************************************************************************/
+    public abstract class WebContext : ApplicationContext
+    {
+        /****************************************************************************/
+        public WebContext()
+        {
+            this.Cache = new Cache();
+            WebUtil.Cache.AddSetting("AppContext", this);
+        }
+
+        /****************************************************************************/
+        public override void Validate(ILog log = null)
+        {
+            // Do nothing
+        }
+
+        /****************************************************************************/
+        public override string MapPath(string path)
+        {
+            return(HttpContext.Current.Server.MapPath(path));
+        }
+
+        /****************************************************************************/
+        public static IAppContext Current
+        {
+            get
+            {
+                return(WebUtil.Cache.GetSetting("AppContext") as IAppContext);
+            }
+        }
+    }
+}
